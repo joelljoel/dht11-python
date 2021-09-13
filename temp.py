59e1e184ff11c67b869e797ee86d840023d6c48a
Joel <72008676+joelss2001@users.noreply.github.com>
2021-09-13 22:44:03 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -5,14 +5,17 @@ import datetime
 # initialize GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
+# iniatilize an optional led at gpio 16 to indicate when the sensor is taking input. commentout otherwise  
 GPIO.setup(16,GPIO.OUT)
 # read data using pin 14
 #now = datetime.datetime.now()
 instance = dht11.DHT11(pin = 4)
+#creating and opening a text file 'temp.txt' for writing temp and humidity
 f1=open("temp.txt","w")
 result = instance.read()
 x=0
 for x in range(24):
+        #led op, commentout otherwise
         GPIO.output(16,True)
         if result.is_valid():
                 f1.write(str(datetime.datetime.now())+"\t"+" temp:"+str(result.temperature)+"  humidity:"+str(result.humidity))     
@@ -20,6 +23,7 @@ for x in range(24):
                 print("Error: %d" % result.error_code)
 
         time.sleep(1)
+        #led op, commentout otherwise
         GPIO.output(16,False)
         time.sleep(1)
         
